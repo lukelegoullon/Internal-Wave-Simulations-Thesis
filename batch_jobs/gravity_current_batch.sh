5ac9ddd34ec41dccfa372f39fca0ef4e42858301
lukelegoullon <lukelegoullon@gmail.com>
2026-08-26 18:19:04 -0600

    Update Batch Job

--- batch_jobs/gravity_current_batch.sh
+++ batch_jobs/gravity_current_batch.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --time=00:10:00
-#SBATCH --partition=atesting
-#SBATCH --qos=testing
-#SBATCH --output=sample-%j.out
+#SBATCH --ntasks=16
+#SBATCH --time=00:30:00
+#SBATCH --partition=acpu
+#SBATCH --qos=cpu-normal
+#SBATCH --output=gravity_current.%j.out
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=lule1957@colorado.edu
 
 module purge
 
-source env-setup.sh
+source ../hpc_configs/env-setup.sh
 
 echo "== This is the scripting step! =="
 sleep 30
-../executable.exe
+/home/lule1957/Internal-Wave-Simulations-Thesis/external/SPINS/src/cases/gravity_current/gravity_current.x
 echo "== End of Job =="
